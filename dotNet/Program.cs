0617b6a2b6162ef690a5d85499c82a791ad6eac6
PantDivya <divyapant108@gmail.com>
2023-07-23 11:55:50 +0545

    Csharp basics and web development basics

---
+++ dotNet/Program.cs
@@ -0,0 +1,27 @@
+﻿class Program
+{
+    static void Main(string[] args)
+     {
+            ABC a = new ABC();
+            // a.CalculateAverageAge();
+     }
+
+}
+public class ABC
+{  
+public double CalculateAverageAge(byte age1, byte age2, byte age3)
+        {
+    double average= (age1+age2+age3)/3;
+    return average;
+        }
+}
+
+public class xyz
+{
+    void Test(){
+        
+       
+        
+    }
+}
+        
